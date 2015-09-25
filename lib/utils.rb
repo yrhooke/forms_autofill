f73530b79aff494ab2b88f04ff9958cfad040367
yrhooke <yotamvonreshef@gmail.com>
2015-09-25 06:49:32 +0300

    really made a mess of things

--- lib/utils.rb
+++ lib/utils.rb
@@ -5,6 +5,8 @@ module FormsAutofill
     blank_path = "./db/1010-blank.pdf"
     indices_path = "./tmp/indices.pdf"
 
+    blank = PdfForms::Pdf.new blank_path, pdftk
+
     # Pdf forms commands are all based on SafeShell.execute which takes comma sepearated arguments, and the pdftk binary  - docs here: https://www.pdflabs.com/docs/pdftk-man-page/
 
     # Commands used via command line to fill pdf:
@@ -15,17 +17,17 @@ module FormsAutofill
     #     pdftk.call_pdftk("./db/1010-blank.pdf", "fill_form", "./tmp/sample2.fdf", "output", "./tmp/filled.pdf")
     # will do the same thing as above
 
-    # class PdfForms::DataFormat
-    #     ## to fix format of fdf
-    #     def to_pdf_data
-    #         pdf_data = header
-    #         @data.each do |form_part|
-    #             pdf_data << field(form_part.name, form_part.value)
-    #         end  
-    #         pdf_data << footer
-    #         return encode_data(pdf_data)
-    #     end  
-    # end  
+    class PdfForms::DataFormat
+        ## to fix format of fdf
+        def to_pdf_data
+            pdf_data = header
+            @data.each do |form_part|
+                pdf_data << field(form_part.name, form_part.value)
+            end  
+            pdf_data << footer
+            return encode_data(pdf_data)
+        end  
+    end  
 
     class PdfForms::PdftkWrapper
         ## to write directly to files from ruby, without any manipulation
@@ -43,7 +45,7 @@ module FormsAutofill
 
     class PdfForms::Field
         # to be able to note some info about fields
-        attr_accessor :meta
+        attr_accessor :meta, :role, :value
     end
 
 
