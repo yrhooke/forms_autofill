5097adc93a5b03c957785765e547ae526ee7da7d
yrhooke <yotamvonreshef@gmail.com>
2015-10-13 03:00:41 +0300

    win

--- bin/win-setup.bat
+++ bin/win-setup.bat
@@ -1,11 +1,11 @@
 
 for /f %%i in ('cd') do set origin=%%i
 
-for /f "delims==" %%i in ('where pdftk') do set /p pdftk1= %%i
+for /f "delims==" %%i in ('where pdftk') do set pdftk1= %%i
 
 cd %~dp0
 
-mklink %pdftk1% bin/pdftk
+mklink %pdftk1% ./bin/pdftk
 
 cd %origin%
 
